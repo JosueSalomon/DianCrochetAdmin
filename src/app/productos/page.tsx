b3f4f745a38e0311506106b5e22b02a46564875a
samuenoc <sreyesz@unah.hn>
2024-11-22 23:09:26 -0600

    cambios sidebar

--- src/app/productos/page.tsx
+++ src/app/productos/page.tsx
@@ -1,27 +1,37 @@
 'use client'
 
-
 import HeaderAdmin from 'components/HeaderAdmin'
-import Sidebar from 'components/Sidebar'
+import SideBar from 'components/Sidebar'
 import Titulo from './components/Titulo'
 import Interactions from './components/Interactions'
 
-export default function productos () {
+export default function Productos() {
     return (
-        <div className="flex h-screen w-full flex-col bg-slate-50 overflow-hidden ">
-            <div className="h-[12%] w-full">
-                <HeaderAdmin/>
+        <div className="flex h-screen w-full flex-col bg-slate-50 overflow-hidden">
+            {/* Header */}
+            <div className="h-[12%] w-full bg-slate-50">
+                <HeaderAdmin />
             </div>
-            
-                <Sidebar/>
 
-            <div className='flex h-full w-full pt-[5%] pl-[18%]'>
-                <div className="h-full z-2 w-full bg-[#COCOCO]">
-                    <div className="h-[8%] w-full">
-                        <Titulo/></div>
-                    <div className="h-full w-full">
-                        <Interactions/></div>
+            {/* Sidebar and Main Content */}
+            <div className="flex h-full w-full bg-slate-50">
+                {/* Sidebar */}
+                <div className="w-[18%] ">
+                    <SideBar />
+                </div>
+
+                {/* Main Content */}
+                <div className="flex flex-col h-full w-full bg-slate-100 p-5">
+                    {/* Title */}
+                    <div className="h-[8%] w-full mb-9 bg-slate-50">
+                        <Titulo />
                     </div>
+
+                    {/* Interactions */}
+                    <div className="h-full w-full bg-slate-50">
+                        <Interactions />
+                    </div>
+                </div>
             </div>
         </div>
     )
