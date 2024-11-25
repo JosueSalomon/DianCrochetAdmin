66d78ad56f4ce9677365e0eba3941d6eb686e413
ale2002solano <ale134valle@gmail.com>
2024-11-25 04:28:56 -0600

    hauto

--- src/app/ordenes/components/posts/DetailsOrdenForm.tsx
+++ src/app/ordenes/components/posts/DetailsOrdenForm.tsx
@@ -223,7 +223,7 @@ if (!detalleOrden || !ordenId) {
         </div>
   </div>
 
-  <div className="w-full bg-white rounded-md p-5 flex flex-col h-full text-gray-950 mt-3">
+  <div className="w-full bg-white rounded-md p-5 flex flex-col h-auto text-gray-950 mt-3">
     <header className="flex justify-center border-b font-rubik font-semibold text-xl text-gray-900">Productos</header>
     <div className="max-h-96 overflow-y-auto mt-2">
     <table>
