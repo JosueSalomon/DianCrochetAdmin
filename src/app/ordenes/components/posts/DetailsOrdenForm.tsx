993b3e77feae9d09d5ac1a7b0c11c1966457f4a9
ale2002solano <ale134valle@gmail.com>
2024-11-25 23:20:35 -0600

    precio unico

--- src/app/ordenes/components/posts/DetailsOrdenForm.tsx
+++ src/app/ordenes/components/posts/DetailsOrdenForm.tsx
@@ -232,7 +232,7 @@ if (!detalleOrden || !ordenId) {
                 <th className="border-b p-3">Nombre producto</th>
                 <th className="border-b p-3">Orden ID</th>
                 <th className="border-b p-3">Cantidad</th>
-                <th className="border-b p-3">Precio</th>
+                <th className="border-b p-3">Precio unitario</th>
                 <th className="border-b p-3">Total</th>
             </tr>
         </thead>
